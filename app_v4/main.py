fda962f8df565136497cbce99da6b77c364950f9
PavelGosling <146619038+PavelGosling@users.noreply.github.com>
2025-02-26 19:31:53 +0300

    Update main.py

--- app_v4/main.py
+++ app_v4/main.py
@@ -1,6 +1,6 @@
 # run.py
 import uvicorn
-from backend.main import app
+from backend.endpoints import app
 
 if __name__ == "__main__":
-    uvicorn.run("backend.main:app", port=8080, reload=True)
+    uvicorn.run("backend.endpoints:app", port=8080, reload=True)
